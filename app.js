b4f00b4d79f24ac7c9827651928ea22dabdf7791
Ujjwal Mahar <ujjwal.mahar01@gmail.com>
2023-11-04 22:03:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World of CI/CD\n');
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}/`);
+});
+
+module.exports = server; 
