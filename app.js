cc2aeae7ec944125099d93a9498898803f7a70ec
Ujjwal Mahar <ujjwal.mahar01@gmail.com>
2023-11-04 23:34:55 +0530

    UPDATED CODE

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('wELCOME TO THE WORLD OF CI/CD !!!\n');
+    res.end('WELCOME TO THE WORLD OF CI/CD !!!\n');
 });
 
 server.listen(port, () => {
